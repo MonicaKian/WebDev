5f15d2107c3f864c7551b6b4a89de5baf76b7758
MonicaKian <parmy050@mymail.unisa.edu.au>
2021-06-15 00:08:41 +0930

    Added code inside AllDrinks.aspx.cs to retrieve drinks from dataModel

--- WebDev/WebDev/Mangement/AllDrinks.aspx.cs
+++ WebDev/WebDev/Mangement/AllDrinks.aspx.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebDev.Models;
 namespace WebDev.Mangement
 {
     public partial class AllDrinks : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            using (Models.DrinksEntities db = new Models.DrinksEntities())
+            using (var db = new DrinksEntities())
             {
-
+                var items = (from d in db.Drinks
+                             orderby d.name ascending
+                             select d
+                            ).ToList();
             }
         }
     }
